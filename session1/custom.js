02ec223c0aaa8917974102b288764a0c8e18f671
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-06 11:06:59 +0200

    s1

---
+++ session1/custom.js
@@ -0,0 +1,170 @@
+// let var const
+
+// let x = 5
+// let x = 3
+// let x = 4
+// console.log(x)
+
+// let x = 3
+
+// if(true){
+//     let x = 6 
+//     console.log(x)
+// }
+
+// console.log(x)
+//break continue
+// for(;;){
+//     console.log("a")
+// }
+
+// while(true){}
+
+//if switch   
+
+//  x y + - * /
+// x = prompt("x")
+/*
+inputs (3)
+*/
+
+// let x = getUserInput("please enter x value: ")
+// let op = getUserInput("please enter opertaor: ")
+// let y = getUserInput("please enter y value: ")
+// console.log(getResult( x , y , op))
+
+// function getUserInput(message){
+//     return prompt(message)
+// }
+// function getResult(val1, val2,op){
+//     let result
+//     switch(op){
+//         case "+": result =  val1+val2; break;
+//         case "*": result =  val1*val2; break;
+//         case "/": result =  val1/val2; break;
+//         case "-": result =  val1-val2; break;
+//         default: result = "Invalid operator"
+//     }
+//     return result
+// }
+
+// if(true){
+//     x()
+//     z()
+// }
+
+// function x(){ console.log("test")}
+
+// const z= function(){console.log("test 1")}
+
+// const a = () => {}
+
+
+// object array
+// let x = [ 1, "a", false, 5.6]
+// x[0]
+
+// let user = {
+//     name: "marwa",
+//     age:36,
+//     gender: "female",
+//     getUserData : function(){
+//         console.log(this);
+//         // console.log(`This user name is ${this.name} and age is ${this.age} and gender is ${this.gender}`);
+//     }
+// }
+
+// user.getUserData()
+
+const images = [
+    {
+    albumId: 1,
+    id: 1,
+    title: "accusamus beatae ad facilis cum similique qui sunt",
+    url: "https://via.placeholder.com/600/92c952",
+    thumbnailUrl: "https://via.placeholder.com/150/92c952"
+    },
+    {
+    albumId: 1,
+    id: 2,
+    title: "reprehenderit est deserunt velit ipsam",
+    url: "https://via.placeholder.com/600/771796",
+    thumbnailUrl: "https://via.placeholder.com/150/771796"
+    },
+    {
+    albumId: 1,
+    id: 3,
+    title: "officia porro iure quia iusto qui ipsa ut modi",
+    url: "https://via.placeholder.com/600/24f355",
+    thumbnailUrl: "https://via.placeholder.com/150/24f355"
+    },
+    {
+    albumId: 1,
+    id: 4,
+    title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+    url: "https://via.placeholder.com/600/d32776",
+    thumbnailUrl: "https://via.placeholder.com/150/d32776"
+    },
+    {
+    albumId: 1,
+    id: 5,
+    title: "natus nisi omnis corporis facere molestiae rerum in",
+    url: "https://via.placeholder.com/600/f66b97",
+    thumbnailUrl: "https://via.placeholder.com/150/f66b97"
+    },
+    {
+    albumId: 1,
+    id: 6,
+    title: "accusamus ea aliquid et amet sequi nemo",
+    url: "https://via.placeholder.com/600/56a8c2",
+    thumbnailUrl: "https://via.placeholder.com/150/56a8c2"
+    },
+    {
+    albumId: 1,
+    id: 7,
+    title: "officia delectus consequatur vero aut veniam explicabo molestias",
+    url: "https://via.placeholder.com/600/b0f7cc",
+    thumbnailUrl: "https://via.placeholder.com/150/b0f7cc"
+    },
+    {
+    albumId: 1,
+    id: 8,
+    title: "aut porro officiis laborum odit ea laudantium corporis",
+    url: "https://via.placeholder.com/600/54176f",
+    thumbnailUrl: "https://via.placeholder.com/150/54176f"
+    },
+    {
+    albumId: 1,
+    id: 9,
+    title: "qui eius qui autem sed",
+    url: "https://via.placeholder.com/600/51aa97",
+    thumbnailUrl: "https://via.placeholder.com/150/51aa97"
+    },
+    {
+    albumId: 1,
+    id: 10,
+    title: "beatae et provident et ut vel",
+    url: "https://via.placeholder.com/600/810b14",
+    thumbnailUrl: "https://via.placeholder.com/150/810b14"
+    }
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
