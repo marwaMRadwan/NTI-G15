e33225ab15eb79b1b1454d9771eba98a78c46e4a
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-10 11:52:28 +0200

    s5

--- session5/yargsApp/utils/users.js
+++ session5/yargsApp/utils/users.js
@@ -1,25 +1,25 @@
 const dealWithData = require("./dealWithData")
-userData = [
-    {ele:"name", default:false},
-    {ele:"phone", default:false},
-    {ele:"age", default:false},
-    {ele:"addresses", default:[]},
-    {ele:"email", default:false},
-    {ele:"url",default:false},
-    {ele:"addedAt",default:dealWithData.timeFormatter(new Date())},
-    {ele:"updatedAt",default:dealWithData.timeFormatter(new Date())}
-]
+userData = require('./validation')
 
 const addUser = (args)=> {
+    let errors = []
     let user = {}
-    userData.forEach(d=> {
-        if(!d.default) return user[d.ele]=args[d.ele]
-        user[d.ele] = d.default
-    })
-    const users = dealWithData.readDataFromJSON("./db/data.json")
-    users.length == 0 ? user.id=1: user.id = users[users.length-1].id+1
-    users.push(user)
-    dealWithData.writeDataToFile("./db/data.json", users)
+    try{
+        userData.forEach(d=> {
+            // console.log(d.invalid(args[d.ele]))
+            if(d.invalid(args[d.ele])) errors.push(d.invalid(args[d.ele]))
+            if(!d.default) return user[d.ele]=args[d.ele]
+            user[d.ele] = d.default
+        })
+        if(errors.length>0) throw new Error(errors)
+        const users = dealWithData.readDataFromJSON("./db/data.json")
+        users.length == 0 ? user.id=1: user.id = users[users.length-1].id+1
+        users.push(user)
+        dealWithData.writeDataToFile("./db/data.json", users)    
+    }
+    catch(e){
+        console.log(e.message)
+    }
 }
 
 module.exports = {addUser}
---
+++ session5/yargsApp/utils/validation.js
@@ -0,0 +1,46 @@
+dealWithData = require("./dealWithData")
+const validtor = require("validator")
+
+const userData = [
+    {
+        ele: "name",
+        default: false,
+        invalid: function (data) {
+            if (data.length < 3) return "name must be more than 3 chars"
+            else return false
+        }
+    },
+    {
+        ele: "phone",
+        default: false,
+        invalid:
+            (data) => !validtor.isMobilePhone(data, ['ar-EG']) ? "invalid phone" : false
+    },
+    {
+        ele: "age",
+        default: false,
+        invalid: (data) => data < 21 ? "invalid age" : false
+
+    },
+    { ele: "addresses", default: [], invalid: (data) => true },
+    {
+        ele: "email", default: false,
+        invalid:
+            (data) => !validtor.isEmail(data, ['ar-EG']) ? "invalid phone" : false
+    },
+    {
+        ele: "url", default: false, invalid:
+            (data) => !validtor.isURL(data, ['ar-EG']) ? "invalid phone" : false
+    },
+    {
+        ele: "addedAt",
+        default: dealWithData.timeFormatter(new Date()),
+        invalid: (data) => true
+    },
+    {
+        ele: "updatedAt",
+        default: dealWithData.timeFormatter(new Date()),
+        invalid: (data) => true
+    }
+]
+module.exports = userData
