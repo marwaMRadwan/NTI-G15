e02ef5899547d3a0f254be6121d884bddefd2972
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-13 10:50:27 +0200

    s6

--- session6/index.js
+++ session6/index.js
@@ -2,17 +2,22 @@ const path = require("path")
 const express = require("express")
 const app = express()
 
-const staticFile = path.join(__dirname, "/public")
+const staticFile = path.join( __dirname, "/public")
 app.use( express.static(staticFile) )
-//routes
-app.get("", (req, res)=>{ 
-    res.send("hello marwa radwan")
-})
+app.set("view engine", "hbs")
 
-app.get("/marwaRadwan", (req,res)=>{
-    res.send({name:"marwa", age:37})
-})
-app.get('/html', (req,res)=>{
-    res.send("<h1>hello</h1>")
+//routes
+// app.get("", (req, res)=>{ 
+//     res.send("hello marwa radwan")
+// })
+
+// app.get("/marwaRadwan", (req,res)=>{
+//     res.send({name:"marwa", age:37})
+// })
+// app.get('/html', (req,res)=>{
+//     res.send("<h1>hello</h1>")
+// })
+app.get("", (req,res)=>{
+    res.render('home', {name:"marwa"})
 })
 app.listen(3000, ()=>console.log("server on http://localhost:3000"))
