c29f9a8573fd024746e5af04ad2469e57fdbf2a3
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-13 11:14:04 +0200

    s6

--- session6/index.js
+++ session6/index.js
@@ -1,11 +1,14 @@
 const path = require("path")
 const express = require("express")
+const hbs = require("hbs")
 const app = express()
-
 const staticFile = path.join( __dirname, "/public")
+const viewsDir = path.join(__dirname, "/frontend/myViews")
+const layoutsDir = path.join(__dirname, "/frontend/layouts")
 app.use( express.static(staticFile) )
 app.set("view engine", "hbs")
-
+app.set("views", viewsDir)
+hbs.registerPartials(layoutsDir)
 //routes
 // app.get("", (req, res)=>{ 
 //     res.send("hello marwa radwan")
@@ -20,4 +23,5 @@ app.set("view engine", "hbs")
 app.get("", (req,res)=>{
     res.render('home', {name:"marwa"})
 })
+app.get("/about", (req,res)=> res.render('about'))
 app.listen(3000, ()=>console.log("server on http://localhost:3000"))
