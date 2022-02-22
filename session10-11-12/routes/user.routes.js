f7d4b9ad64b5bc91f0c48f02fa2e3e75a02be09c
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-22 09:24:14 +0200

    s12

---
+++ session10-11-12/routes/user.routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+const userController = require("../app/controller/user.controller")
+const auth = require('../middleware/auth')
+router.post("/register", userController.register)
+router.post("/login", userController.login)
+router.get("/all", auth , userController.getAll)
+router.get("/all/:id",auth, userController.getSingle)
+router.delete("/all",auth, userController.delAll)
+router.delete("/all/:id",auth, userController.delSingle)
+router.get("/me", auth , userController.me)
+router.post("/logout", auth,userController.logout)
+router.post("/logoutAll", auth,userController.logoutAll)
+module.exports= router
