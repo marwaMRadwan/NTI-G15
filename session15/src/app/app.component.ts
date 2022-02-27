e7fd604fdf21f91c4827158e2553d5fa2e9cb5fd
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-27 14:52:08 +0200

    s15

--- session15/src/app/app.component.ts
+++ session15/src/app/app.component.ts
@@ -109,5 +109,106 @@ export class AppComponent {
   //   this.btnText=="show"? this.btnText="hide": this.btnText="show"
   //   this.status = !this.status
   // }
-  moNum : number = 1
+  // moNum : number = 1
+  // x = ["monkey","rooster","dog","big","rat","ox","tiiger","rabbit","dragon","snake","horse","sheep"]
+  // yOfBirth: number = 2000
+  data: any[] = [
+    {
+    userId: 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 4,
+    title: "et porro tempora",
+    completed: true
+    },
+    {
+    userId: 1,
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 6,
+    title: "qui ullam ratione quibusdam voluptatem quia omnis",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 7,
+    title: "illo expedita consequatur quia in",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 8,
+    title: "quo adipisci enim quam ut ab",
+    completed: true
+    },
+    {
+    userId: 1,
+    id: 9,
+    title: "molestiae perspiciatis ipsa",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 10,
+    title: "illo est ratione doloremque quia maiores aut",
+    completed: true
+    },
+    {
+    userId: 1,
+    id: 11,
+    title: "vero rerum temporibus dolor",
+    completed: true
+    },
+    {
+    userId: 1,
+    id: 12,
+    title: "ipsa repellendus fugit nisi",
+    completed: true
+    },
+    {
+    userId: 1,
+    id: 13,
+    title: "et doloremque nulla",
+    completed: false
+    },
+    {
+    userId: 1,
+    id: 14,
+    title: "repellendus sunt dolores architecto voluptatum",
+    completed: true
+    },
+    {
+    userId: 1,
+    id: 15,
+    title: "ab voluptatum amet voluptas",
+    completed: true
+    }
+    ]
+    mainData = this.data  
+    serKey:string=""
+  filterData(){
+    this.data = this.mainData.filter(p=>{
+      return p.title.includes(this.serKey)
+    })
+  }
 }
