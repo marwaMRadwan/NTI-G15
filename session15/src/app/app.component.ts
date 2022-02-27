f8a9f7e2cb7bff1beba0d5de90617488da96b27a
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-27 11:19:50 +0200

    s15

--- session15/src/app/app.component.ts
+++ session15/src/app/app.component.ts
@@ -6,28 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'session15';
-  x : number = 5
-  y : number = 4
-  imgSrc:string = "assets/logo.png"
-  content:string = "<div class='alert alert-primary'>hi </div>"
-  hello():string{
-    return "hello from ts"
-  }
-  testEvent():void{
-    console.log("hello")
-    this.x++
-  }
-  testInput(event:Event):void{
-    console.log(event)
-    // console.log(event.target?['value'])
-    let a = event.target as HTMLInputElement
-    console.log(a.value)
-  }
-  testInput1(event:any):void{
-    // console.log(event.target?['value'])
-    let a = event.target.value
-    console.log(a)
-  }
+  // title = 'session15';
+  // x : number = 5
+  // y : number = 4
+  // imgSrc:string = "assets/logo.png"
+  // content:string = "<div class='alert alert-primary'>hi </div>"
+  // hello():string{
+  //   return "hello from ts"
+  // }
+  // testEvent():void{
+  //   console.log("hello")
+  //   this.x++
+  // }
+  // testInput(event:Event):void{
+  //   console.log(event)
+  //   // console.log(event.target?['value'])
+  //   let a = event.target as HTMLInputElement
+  //   console.log(a.value)
+  // }
+  // testInput1(event:any):void{
+  //   // console.log(event.target?['value'])
+  //   let a = event.target.value
+  //   console.log(a)
+  // }
+  testVal:string=""
 
 }
