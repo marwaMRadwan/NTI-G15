a0944d6218d4d6b34de4b776f7b4fd02a360a54d
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-27 10:58:51 +0200

    s15

--- session15/src/app/app.component.ts
+++ session15/src/app/app.component.ts
@@ -9,18 +9,25 @@ export class AppComponent {
   title = 'session15';
   x : number = 5
   y : number = 4
-  imgSrc = "assets/logo.png"
-  content = "<div class='alert alert-primary'>hi </div>"
+  imgSrc:string = "assets/logo.png"
+  content:string = "<div class='alert alert-primary'>hi </div>"
   hello():string{
     return "hello from ts"
   }
-  testEvent(){
+  testEvent():void{
     console.log("hello")
     this.x++
   }
-  testInput(event:Event){
+  testInput(event:Event):void{
+    console.log(event)
     // console.log(event.target?['value'])
     let a = event.target as HTMLInputElement
     console.log(a.value)
   }
+  testInput1(event:any):void{
+    // console.log(event.target?['value'])
+    let a = event.target.value
+    console.log(a)
+  }
+
 }
