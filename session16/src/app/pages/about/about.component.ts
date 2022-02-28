d6dc69d43be0dabd6e5e95c217c89976df2aa8e5
marwaMRadwan <marwaradwan872@hotmail.com>
2022-02-28 13:22:36 +0200

    s16

--- session16/src/app/pages/about/about.component.ts
+++ session16/src/app/pages/about/about.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Users } from 'src/app/models/users';
+import { DataService } from 'src/app/services/data.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  users:Users[]=[]
+  constructor(private _data:DataService) { }
 
   ngOnInit(): void {
+    this._data.getUsers().subscribe(data=>{
+      console.log(data)
+      this.users=data
+    })
   }
 
 }
