2e482ecf97b92ee36ac362d38080f006fd324e62
marwaMRadwan <marwaradwan872@hotmail.com>
2022-03-02 10:42:35 +0200

    s18

--- session17-18/src/app/pages/user/login/login.component.ts
+++ session17-18/src/app/pages/user/login/login.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isSubmitted = false
   loginForm: FormGroup = new FormGroup({
-    email: new FormControl(),
-    password:new FormControl()
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password:new FormControl('', [Validators.minLength(3), Validators.maxLength(20)])
   })
+  get email(){return this.loginForm.get('email')}
+  get password(){return this.loginForm.get('password')}
   constructor() { }
 
   ngOnInit(): void {
   }
   login(){
+    this.isSubmitted=true
     console.log(this.loginForm.value)
   }
 }
